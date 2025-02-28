2601a1c8b2cf48fda39a0ad8f95496950360b62b
isaac <issaktoo2@gmail,com>
2025-02-28 16:41:36 +0300

    requirment.txt

--- Backend /app.py
+++ Backend /app.py
@@ -1,6 +1,5 @@
 from config import create_app
 from flask_jwt_extended import JWTManager
-from flask_cors import CORS
 
 app = create_app()
 
@@ -9,5 +8,5 @@ jwt = JWTManager(app)
 from routes import routes_bp;
 app.register_blueprint(routes_bp)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__== '__main__':
+    app.run(debug=True)
